f4ebe62d5e25de5fc31a175b3a91753c652fc7e7
denisfl <delchyve@gmail.com>
2020-02-26 11:14:04 +0400

    Add lost files

---
+++ db/tags/index.js
@@ -0,0 +1,19 @@
+const faker = require('faker')
+
+const tags = []
+
+for (let i = 1; i < 100; i++) {
+  const firstName = faker.name.firstName()
+
+  const tag = {
+    id: i,
+    text: firstName,
+    platform_id: 2,
+    matters_total: Math.floor(Math.random() * Math.floor(10)),
+  }
+  const item = Object.assign({}, tag)
+  item.id = i
+  tags.push(item)
+}
+
+module.exports = tags
---
+++ db/tags/tag.js
@@ -0,0 +1,6 @@
+module.exports = {
+  id: 1,
+  text: '',
+  platform_id: 2,
+  matters_total: 1,
+}
